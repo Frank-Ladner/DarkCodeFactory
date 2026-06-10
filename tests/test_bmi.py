afabd63fe1bf59885992576cd0d31d1aeda86bcf
Frank Ladner <frank@ladner-online.de>
2026-06-10 02:35:25 +0200

    Unit Test Erweiterung

--- tests/test_bmi.py
+++ tests/test_bmi.py
@@ -1,21 +1,22 @@
-from math import isclose
+import pytest
 
 from app.bmi import calculate_bmi, classify_bmi
 
 
 def test_calculate_bmi():
-    assert isclose(calculate_bmi(170, 70.0), 24.22145, rel_tol=0.00001)
+    assert calculate_bmi(170, 70.0) == pytest.approx(24.22145)
 
 
-def test_classify_bmi():
-    cases = [
+@pytest.mark.parametrize(
+    ("bmi", "expected_category", "expected_color"),
+    [
         (18.4, "Untergewicht", "blue"),
         (18.5, "Normalgewicht", "green"),
         (24.9, "Normalgewicht", "green"),
         (25.0, "Übergewicht", "orange"),
         (29.9, "Übergewicht", "orange"),
         (30.0, "Adipositas", "red"),
-    ]
-
-    for bmi, expected_category, expected_color in cases:
-        assert classify_bmi(bmi) == (expected_category, expected_color)
+    ],
+)
+def test_classify_bmi_boundaries(bmi, expected_category, expected_color):
+    assert classify_bmi(bmi) == (expected_category, expected_color)
